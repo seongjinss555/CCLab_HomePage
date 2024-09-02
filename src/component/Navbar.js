aa8902451193a1825900b0de66f78f1fe69d3df1
seongjinss555 <khksj0109@gmail.com>
2024-09-02 12:52:15 +0900

    navbar modified

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,30 +5,49 @@ import "./Navbar.css";
 function Navbar() {
   const [click, setClick] = useState(false);
   const [dropdown, setDropdown] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             CCLab.
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/introduction" className="nav-links">
+              <Link
+                to="/introduction"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Introduction
               </Link>
             </li>
-            <li className="nav-item">
-              <li className="nav-links">Researchers</li>
+            <li className="nav-item" onClick={() => setDropdown(!dropdown)}>
+              <span className="nav-links">Researchers</span>
               {dropdown && (
                 <ul className="dropdown-menu">
                   <li>
-                    <Link to="/professor" className="dropdown-link">
+                    <Link
+                      to="/professor"
+                      className="dropdown-link"
+                      onClick={closeMobileMenu}
+                    >
                       Professor
                     </Link>
                   </li>
                   <li>
-                    <Link to="/researchers" className="dropdown-link">
+                    <Link
+                      to="/researchers"
+                      className="dropdown-link"
+                      onClick={closeMobileMenu}
+                    >
                       Students
                     </Link>
                   </li>
@@ -36,12 +55,20 @@ function Navbar() {
               )}
             </li>
             <li className="nav-item">
-              <Link to="/achievements" className="nav-links">
+              <Link
+                to="/achievements"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Achievements
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/comments" className="nav-links">
+              <Link
+                to="/comments"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Contact Us
               </Link>
             </li>
