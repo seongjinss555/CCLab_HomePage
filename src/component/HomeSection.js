799a96c78da92b6a5627d03cc22bcad2c46b0bcb
seongjinss555 <khksj0109@gmail.com>
2024-09-02 13:18:29 +0900

    video changed

--- src/component/HomeSection.js
+++ src/component/HomeSection.js
@@ -5,7 +5,7 @@ import "./HomeSection.css";
 function HomeSection() {
   return (
     <div className="home-container">
-      <video src="/videos/video1.mp4" type="video/mp4" autoPlay loop muted />
+      <video src="/videos/video.mp4" type="video/mp4" autoPlay loop muted />
       <h1>컴퓨터 네트워크 연구실에 오신 걸 환영합니다!</h1>
       <Button />
     </div>
