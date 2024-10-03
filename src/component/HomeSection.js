e12be5233f7549326daf7b424ceb51fd819ad2ca
seongjinsss <khksj0109@gmail.com>
2024-10-03 21:44:07 +0900

    homesection changed

--- src/component/HomeSection.js
+++ src/component/HomeSection.js
@@ -1,20 +1,58 @@
 import React from "react";
-import { Button } from "./Button";
-import "./HomeSection.css";
+import styled from "styled-components";
+import TypewriterComponent from "typewriter-effect";
+
+const TypewriterBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px;
+  border: 2px solid #000;
+  border-radius: 15px;
+  background-color: #fff;
+  width: 900px;
+  margin: 20px 0;
+  font-size: 38px;
+`;
+
+const HomeContainer = styled.div`
+  display: flex;
+
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* 전체 화면을 차지 */
+  background-color: #f8f9fa;
+  font-family: Arial, sans-serif;
+`;
+
+const HomeTitle = styled.h1`
+  font-size: 48px;
+  margin-bottom: 20px;
+`;
+
+const ImageContainer = styled.img`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`;
 
 function HomeSection() {
   return (
-    <div className="home-container">
-      <div className="image-container">
-        <img
-          src="/index/index.jpg"
-          alt="컴퓨터 네트워크 연구실"
-          className="background-image"
+    <HomeContainer>
+      <HomeTitle>CCLab.</HomeTitle>
+      <TypewriterBox>
+        <TypewriterComponent
+          options={{
+            strings: ["컴퓨터 네트워크 연구실에 오신 걸 환영합니다!"],
+            autoStart: true,
+            loop: true,
+            delay: 65,
+          }}
         />
-        <h1>컴퓨터 네트워크 연구실에 오신 걸 환영합니다!</h1>
-        <Button />
-      </div>
-    </div>
+      </TypewriterBox>
+    </HomeContainer>
   );
 }
 
